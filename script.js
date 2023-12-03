44ae62634879a89243835378a351b52444f05a81
ARustam11 <93269378+ARustam11@users.noreply.github.com>
2023-12-02 19:42:23 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-const swiper = new Swiper(".swiper", {
-  // pagination: {
-  //   el: ".swiper-pagination",
-  // },
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".slides-container");
+  const slides = document.querySelectorAll(".slide");
+  let currentIndex = 0;
+ 
+  function moveToNextSlide() {
+     currentIndex = (currentIndex + 1) % slides.length;
+     slider.style.transition = "transform 3s ease-in-out";
+     slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+  }
+ 
+  setInterval(moveToNextSlide, 8000);
+ });
+
+ const buttons = document.querySelectorAll("[data-carousel-button]")
+ buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+    .closest("[data-carousel]")
+    .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+ })
